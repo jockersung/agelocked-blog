1a1d17d344f13fa15992a27cd35997077a93666e
jockersung <sunyikun0729@gmail.com>
2026-04-03 21:08:58 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,13 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://www.agelocked.com',
-	integrations: [mdx(), sitemap()],
+	integrations: [
+		mdx(),
+		sitemap({
+			serialize(item) {
+				item.lastmod = new Date().toISOString();
+				return item;
+			},
+		}),
+	],
 });
